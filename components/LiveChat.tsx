57e4a795ca2134694bc47d01acdefab318208b2e
Cyber <shaibuafeez@gmail.com>
2025-12-27 13:51:05 +0100

    fix: hide gift button for broadcasters

--- components/LiveChat.tsx
+++ components/LiveChat.tsx
@@ -245,23 +245,25 @@ export default function LiveChat({ roomName, isBroadcaster = false, streamId, cr
               focus:bg-gray-800/90 transition-colors"
           />
 
-          {/* Gift Icon Button */}
-          <button
-            type="button"
-            onClick={() => setIsDonationModalOpen(true)}
-            disabled={!currentAccount?.address}
-            className="w-10 h-10 flex-shrink-0 bg-white/90
-              backdrop-blur-sm rounded-full
-              flex items-center justify-center
-              hover:bg-white
-              disabled:opacity-50 disabled:cursor-not-allowed
-              transition-all"
-            title="Send Gift"
-          >
-            <svg className="w-5 h-5 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
-            </svg>
-          </button>
+          {/* Gift Icon Button - Only show for viewers, not broadcaster */}
+          {!isBroadcaster && (
+            <button
+              type="button"
+              onClick={() => setIsDonationModalOpen(true)}
+              disabled={!currentAccount?.address}
+              className="w-10 h-10 flex-shrink-0 bg-white/90
+                backdrop-blur-sm rounded-full
+                flex items-center justify-center
+                hover:bg-white
+                disabled:opacity-50 disabled:cursor-not-allowed
+                transition-all"
+              title="Send Gift"
+            >
+              <svg className="w-5 h-5 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 8v13m0-13V6a2 2 0 112 2h-2zm0 0V5.5A2.5 2.5 0 109.5 8H12zm-7 4h14M5 12a2 2 0 110-4h14a2 2 0 110 4M5 12v7a2 2 0 002 2h10a2 2 0 002-2v-7" />
+              </svg>
+            </button>
+          )}
 
           {/* Send Button */}
           <button
